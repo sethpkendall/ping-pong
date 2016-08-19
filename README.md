ea8aff467c8c55ac1e12629cc9ef34543566f390
sethpkendall <sethpkendall@gmail.com>
2016-08-19 09:56:44 -0700

    build out backend logic

--- README.md
+++ README.md
@@ -12,15 +12,15 @@
   * Example Input: word
   * Example Output: null
 
-* For numbers divisible by 3, the program inserts the word "ping" instead.
-  * Example Input: 5
-  * Example Output: [1, 2, ping, 4, 5]
+* For numbers divisible by 3, it inserts the word "ping" instead.
+  * Example Input: 6
+  * Example Output: [1, 2, ping, 4, 5, 6]
 
-* For numbers divisible by 5, the program inserts the word "pong" instead.
+* For numbers divisible by 5, it inserts the word "pong" instead.
   * Example Input: 5
   * Example Output: [1, 2, ping, 4, pong]
 
-* For numbers divisible by 15, the program inserts the word "pingpong" instead.
+* For numbers divisible by 15, it inserts the word "pingpong" instead.
   * Example Input: 20
   * Example Output: [1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, pingpong, 16, 17, ping, 19, pong]
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
+var pingInsert = function(array1){
+  var pingPonged1 = array1.map(function(x) {
+    if ((x%5===0)&&(x%3===3)) {
+      pingPonged1 += "pingpong";
+    } else if (x%3===0) {
+      pingPonged1 += "ping";
+    } else if (x%5===0) {
+      pingPonged1 += "pong";
+    } else {
+      console.log("something");
+    }
+    return pingPonged1;
+  });
+};
+
+
 // Back end
+var countTo = function(num) {
+  var inArray = [];
+  for (i=0;i<=num;i++) {
+    inArray += i;
+  }
+  console.log(inArray);
+  var inArray2 = inArray.slice(1,num+1);
+  console.log(inArray2);
+  var pingPonged2 = pingInsert(inArray2);
+  console.log(pingPonged2);
+  return pingPonged2;
+};
 
 // Front end
+$(document).ready(function(){
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    var lastNum = parseInt($("input#original").val());
+    if(lastNum){
+      var result = countTo(lastNum);
+      $("#output").text(result);
+    } else {
+      alert("Please enter a number!");
+    }
+  });
+});
